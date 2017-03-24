581a7794a6ac504c9179233cf7a7e815356eaa7c
esamarthur <esamarthur@outlook.com>
2017-03-24 07:54:01 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+<style type="text/css">
+	img{
+		float: right;
+		margin-top: -20px;
+		height: 648px;
+		width: 1400px;
+	}
+	.navbar-default{
+		background-color: #efeff4;
+		border-color: #efeff4;
+		color: #FFFFFF;
+	}
+	.navbar-inverse{
+		background-color: #efeff4;
+		border-color: #efeff4;
+		color: #FFFFFF;
+	}
+
+</style>
+
+</head>
+<body class="">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="index.html">Exam Hall Seat Allocation System</a>
+	
+	<div class="navbar-header navbar-right">
+		<ul class="nav navbar-nav">
+			
+			<li><a href="adminlogin">Admin</a></li>
+			<li><a href="stlogin">Student</a></li>
+			
+      </ul>
+	</div>
+ </div>
+</nav>
+
+<div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+	<div class="container">
+		<div class="navbar-text pull-left">
+			<p>&copy2017 Gp9-Software Enginnering Project.</p>
+		</div>
+	</div>
+	
+</div>
+
+
+<!--<img src="images/student.jpg">-->
+<script src="js/jQuery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
